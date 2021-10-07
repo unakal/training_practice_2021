e5736ea1cf46e3a2b301af3a5dbcac1cb996df9c
Naveen <unakalnaveen@gmail.com>
2021-10-07 12:43:19 +0530

    REQ-104 -Created a Book Controller as per the book service requirement

--- book_service/src/main/java/com/example/demo/BookServiceApplication.java
+++ book_service/src/main/java/com/example/demo/BookServiceApplication.java
@@ -9,6 +9,7 @@ public class BookServiceApplication {
 
 	public static void main(String[] args) {
 	ConfigurableApplicationContext ctx = SpringApplication.run(BookServiceApplication.class, args);
+	System.out.println("Done");
 	ctx.close();
 	}
 
